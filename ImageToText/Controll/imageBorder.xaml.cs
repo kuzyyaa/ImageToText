2325cd110aee3a1613348a0a42df44f6934b75a8
Кузнецов Игорь <kuznetsovpro3211@yandex.ru>
2023-01-25 18:24:28 +0300

    Добавьте файлы проекта.

---
+++ ImageToText/Controll/imageBorder.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageToText.Controll
+{
+    /// <summary>
+    /// Логика взаимодействия для imageBorder.xaml
+    /// </summary>
+    public partial class imageBorder : UserControl
+    {
+        public imageBorder()
+        {
+            InitializeComponent();
+        }
+
+        public imageBorder(Grid gr, Image image)
+        {
+            InitializeComponent();
+            border.Height = 100;
+            border.Width = 200;
+            this.image = image;
+            this.Width = gr.Width;
+            this.Height = gr.Height;
+            grImage = gr;
+        }
+        Grid grImage;
+        Image image;
+        Point _positionInBlock;
+        bool flagResize = false;
+        private void UserControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            if (this.IsMouseCaptured && !flagResize)
+            {
+                var container = VisualTreeHelper.GetParent(this) as UIElement;
+
+                if (container == null)
+                    return;
+
+                var mousePosition = e.GetPosition(container);
+                this.RenderTransform = new TranslateTransform(mousePosition.X - _positionInBlock.X, mousePosition.Y - _positionInBlock.Y);
+            }
+        }
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _positionInBlock = Mouse.GetPosition(this);
+            this.CaptureMouse();
+        }
+        private void UserControl_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.ReleaseMouseCapture();
+        }
+        private void border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            flagResize = true;
+        }
+        private void border_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            flagResize = false;
+        }
+        private void border_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (flagResize)
+            {
+                var mousePosition = e.GetPosition(border);
+                border.Width = mousePosition.X+20;
+                border.Height = mousePosition.Y+20;
+            }
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            grImage.Children.Remove(this);
+        }
+    }
+}
---
+++ ImageToText/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using Tesseract;
+using Spire.Pdf;
+using static System.Net.WebRequestMethods;
+using Point = System.Windows.Point;
+
+namespace ImageToText
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        TesseractEngine ocrengine = new TesseractEngine(@".\tessdata", "rus+eng", EngineMode.Default);
+
+        /// <summary>
+        /// Добавить рамку
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            grBorder.Children.Add(new Controll.imageBorder(grBorder, imageControl)) ;
+        }
+        //Распознать
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            tbResult.Text = "";
+
+            if (!Directory.Exists("Images"))
+            {
+                Directory.CreateDirectory("Images");
+            }
+
+            System.IO.DirectoryInfo di = new DirectoryInfo("Images");
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+
+            int index = 0;
+            foreach (Controll.imageBorder item in grBorder.Children)
+            {
+                Point relativePoint = item.border.TransformToAncestor(grBorder)
+                              .Transform(new Point(0, 0));
+                var fullBitmap = imageControl.Source as BitmapImage;
+                BitmapImage source = new BitmapImage();
+                source.BeginInit();
+                source.UriSource = fullBitmap.UriSource;
+                source.DecodePixelHeight = (int)grBorder.ActualHeight;
+                source.DecodePixelWidth = (int)grBorder.ActualWidth;
+                source.EndInit();
+                var result = new CroppedBitmap(source, new Int32Rect((int)relativePoint.X, (int)relativePoint.Y, (int)item.border.Width, (int)item.border.Height));
+                PngBitmapEncoder pngImage = new PngBitmapEncoder();
+                pngImage.Frames.Add(BitmapFrame.Create(result));           
+                using (Stream fileStream = System.IO.File.Create(@"Images\" + index+".png"))
+                {
+                    pngImage.Save(fileStream);
+                }
+                index++;
+            }
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                var result = GetText(file.FullName);
+                tbResult.Text += $"{result}\n";
+            }
+        }
+        /// <summary>
+        /// Возвращает строку из куска фото
+        /// </summary>
+        /// <param name="urlImage"></param>
+        /// <returns></returns>
+        private string GetText(string urlImage)
+        {
+            var img = Pix.LoadFromFile(urlImage);
+            using (var page = ocrengine.Process(img, PageSegMode.Auto))
+            {
+                return page.GetText();
+            }
+        }
+        /// <summary>
+        /// Выбор файла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbFile_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                tbFile.Text = openFileDialog.FileName; 
+                imageControl.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+
+            }
+        }
+
+        /// <summary>
+        /// PDF Convert
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ///Выбираем файл PDF
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Pdf Files|*.pdf";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                PdfDocument pdf = new PdfDocument();
+                pdf.LoadFromFile(openFileDialog.FileName);
+                for (int i = 0; i < pdf.Pages.Count; i++)
+                {
+                    var source = pdf.SaveAsImage(i);
+                    source.Save($"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\\PdfConvert{i}.png", System.Drawing.Imaging.ImageFormat.Png);
+                }
+
+                MessageBox.Show("Все изображения сохранены на рабочий стол.", "Information");
+            }
+        }
+    }
+}
